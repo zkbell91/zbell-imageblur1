59d60e98455fd764f88f81afd31f3efb4c93c219
zkbell91 <49664973+zkbell91@users.noreply.github.com>
2019-07-14 02:30:31 -0400

    Initial Commit
    Upload code for Image Blur #1

---
+++ image_blur_1.rb
@@ -0,0 +1,27 @@
+class Image
+
+
+  def initialize (picture)
+    @picture = picture
+  end
+
+
+  def output_image
+    @picture.each do |x|
+      x.each do |cell|
+      end
+      puts x.join(" ")
+    end
+  end
+end
+
+
+image = Image.new([
+  [0, 0, 0, 0],
+  [0, 1, 0, 0],
+  [0, 0, 0, 1],
+  [0, 0, 0, 0]
+])
+
+ 
+image.output_image
